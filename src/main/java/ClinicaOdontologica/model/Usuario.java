960590c62e3eae9071645ed41b51fc84984159e1
gloufac <lpez.gloria2@gmail.com>
2024-06-17 23:36:22 -0500

    Listo funcionando como debe ser

--- src/main/java/ClinicaOdontologica/model/Usuario.java
+++ src/main/java/ClinicaOdontologica/model/Usuario.java
@@ -94,7 +94,7 @@ public class Usuario implements UserDetails {
      */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(usuarioRole.toString());
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(usuarioRole.name());
         return Collections.singletonList(simpleGrantedAuthority);
     }
 
